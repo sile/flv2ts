731d98fb2d05694ca71483194f3a4c591aa45ccd
Takeru Ohta <phjgt308@gmail.com>
2012-11-25 12:03:09 +0900

    音声データ変換追加中

--- src/bin/flv2ts.cc
+++ src/bin/flv2ts.cc
@@ -18,10 +18,12 @@ const static unsigned ES_AUDIO_PID = 0x101;
 const static unsigned STREAM_TYPE_VIDEO = 27;
 const static unsigned STREAM_TYPE_AUDIO = 15;
 const static unsigned VIDEO_STREAM_ID = 224;
+const static unsigned AUDIO_STREAM_ID = 192;
 const static unsigned PMT_TABLE_ID = 2;
 const static double PTS_DTS_OFFSET = 0.1;
 
 static unsigned g_counter = 0; // XXX:
+static unsigned g_audio_counter = 0; // XXX:
 static uint64_t g_prev_pcr = 0;
 
 void write_ts_pat(std::ostream& out) {
@@ -132,11 +134,9 @@ void write_ts_pmt(std::ostream& out) {
     pmt.stream_info_list.push_back(info);
 
     // audio
-    /*
     info.stream_type    = STREAM_TYPE_AUDIO;
     info.elementary_pid = ES_AUDIO_PID;
     pmt.stream_info_list.push_back(info);
-    */
   }
 
   pmt.section_length = 13 + pmt.program_info_length + pmt.stream_info_list.size() * 5;
@@ -171,7 +171,8 @@ uint64_t sec_to_90kHz(double sec) {
   return static_cast<uint64_t>(sec * 90000.0);
 }
 
-void write_video_first(const flv::Tag& tag, const std::string& payload, std::ostream& out, size_t& data_offset) {
+void write_video_first(const flv::Tag& tag, const std::string& payload, std::ostream& out, size_t& data_offset,
+                       size_t stuffing_byte_size=0) {
   const flv::VideoTag& video = tag.video;
 
   char buf[256];
@@ -200,7 +201,7 @@ void write_video_first(const flv::Tag& tag, const std::string& payload, std::ost
   af.discontinuity_indicator = false;
   af.random_access_indicator = (video.frame_type == flv::VideoTag::FRAME_TYPE_KEY);
   af.es_priority_indicator =  false;
-  af.pcr_flag = true;
+  af.pcr_flag = true; // XXX: 毎回書く必要はない (100msに一回)
   af.opcr_flag = false;
   af.splicing_point_flag = false;
   af.transport_private_data_flag = false;
@@ -210,7 +211,7 @@ void write_video_first(const flv::Tag& tag, const std::string& payload, std::ost
   af.pcr = (pcr << 15) + (0x3F << 9);
   g_prev_pcr = pcr;
 
-  af.adaptation_field_length = 7;
+  af.adaptation_field_length = 7 + stuffing_byte_size;
   
   size = af.dump(buf + wrote_size, sizeof(buf) - wrote_size);
   wrote_size += size;
@@ -244,9 +245,18 @@ void write_video_first(const flv::Tag& tag, const std::string& payload, std::ost
 
   size = pes.dump(buf + wrote_size, sizeof(buf) - wrote_size);
   wrote_size += size;
-  out.write(reinterpret_cast<const char*>(buf), wrote_size);
 
-  // TODO: video.paylaod_size < (ts::Packet::SIZE - wrote_size) の場合の処理
+  // video.paylaod_size < (ts::Packet::SIZE - wrote_size) の場合の処理
+  if(payload.size() < (ts::Packet::SIZE - wrote_size)) {
+    size_t delta =  (ts::Packet::SIZE - wrote_size) - payload.size();
+    // XXX: 暫定
+    data_offset = 0;
+    g_counter--;
+    write_video_first(tag, payload, out, data_offset, delta);
+    return;
+  }
+
+  out.write(reinterpret_cast<const char*>(buf), wrote_size);
 
   pes.data_size = ts::Packet::SIZE - wrote_size;
   pes.data = reinterpret_cast<const uint8_t*>(payload.data());
@@ -322,6 +332,182 @@ void write_video(const flv::Tag& tag, const std::string& payload, std::ostream& 
   }
 }
 
+void write_audio_first(const flv::Tag& tag, const std::string& payload, std::ostream& out, size_t& data_offset,
+                       size_t stuffing_byte_size=0) {
+  char buf[256];
+  ssize_t size;
+  size_t wrote_size = 0;
+
+  // sync-byte
+  buf[0] = static_cast<char>(ts::Packet::SYNC_BYTE);
+  wrote_size += 1;
+
+  // header
+  ts::Header h;
+  h.transport_error_indicator = false;
+  h.payload_unit_start_indicator = true;
+  h.transport_priority = 0;
+  h.pid = ES_AUDIO_PID;
+  h.scrambling_control = 0;
+  h.adaptation_field_exist = 3; // payload and adaptation_field
+  h.continuity_counter = (g_audio_counter++) % 16; // XXX:
+  
+  size = h.dump(buf + wrote_size, sizeof(buf) - wrote_size);
+  wrote_size += size;
+  
+  // adaptation_field
+  ts::AdaptationField af;
+  af.discontinuity_indicator = false;
+  af.random_access_indicator = true;
+  af.es_priority_indicator =  false;
+  af.pcr_flag = false;
+  af.opcr_flag = false;
+  af.splicing_point_flag = false;
+  af.transport_private_data_flag = false;
+  af.adaptation_field_extension_flag = false;
+
+  af.adaptation_field_length = 7 + stuffing_byte_size;
+  
+  size = af.dump(buf + wrote_size, sizeof(buf) - wrote_size);
+  wrote_size += size;
+  
+  // pes
+  ts::PES pes;
+  pes.packet_start_prefix_code = 1;
+  pes.stream_id = AUDIO_STREAM_ID;
+
+  const unsigned optional_header_size = 13;
+  pes.pes_packet_length = optional_header_size + payload.size(); // XXX: video.payload_size が 2byte を超過する時がある
+
+  ts::OptionalPESHeader& oph = pes.optional_header;
+  oph.marker_bits = 2;
+  oph.scrambling_control = 0;
+  oph.priority = 0;
+  oph.data_alignment_indicator = false;
+  oph.copyright = false;
+  oph.original_or_copy = false;
+  oph.pts_indicator = true;
+  oph.dts_indicator = true;
+  oph.escr_flag = false;
+  oph.es_rate_flag = false;
+  oph.dsm_trick_mode_flag = false;
+  oph.additional_copy_info_flag = false;
+  oph.crc_flag = false;
+  oph.extension_flag = false;
+  oph.pes_header_length = 10;
+  oph.dts = sec_to_90kHz(static_cast<double>(tag.timestamp) / 1000.0 + PTS_DTS_OFFSET);
+  oph.pts = oph.dts;
+
+  size = pes.dump(buf + wrote_size, sizeof(buf) - wrote_size);
+  wrote_size += size;
+
+  // audio.paylaod_size < (ts::Packet::SIZE - wrote_size) の場合の処理
+  if(payload.size() < (ts::Packet::SIZE - wrote_size)) {
+    size_t delta =  (ts::Packet::SIZE - wrote_size) - payload.size();
+    // XXX: 暫定
+    data_offset = 0;
+    g_audio_counter--;
+    write_audio_first(tag, payload, out, data_offset, delta);
+    return;
+  }
+
+  out.write(reinterpret_cast<const char*>(buf), wrote_size);
+
+  pes.data_size = ts::Packet::SIZE - wrote_size;
+  pes.data = reinterpret_cast<const uint8_t*>(payload.data());
+  out.write(reinterpret_cast<const char*>(pes.data), pes.data_size);
+
+  data_offset += pes.data_size;
+}
+
+void write_audio_rest(const flv::Tag& tag, const std::string& payload, std::ostream& out, size_t& data_offset) {
+  char buf[256];
+  ssize_t size;
+  size_t wrote_size = 0;
+
+  // sync-byte
+  buf[0] = static_cast<char>(ts::Packet::SYNC_BYTE);
+  wrote_size += 1;
+
+  // header
+  ts::Header h;
+  h.transport_error_indicator = false;
+  h.payload_unit_start_indicator = false;
+  h.transport_priority = 0;
+  h.pid = ES_AUDIO_PID;
+  h.scrambling_control = 0;
+  h.adaptation_field_exist = 1; // payload only
+  h.continuity_counter = (g_audio_counter++) % 16;
+  
+  if(payload.size() - data_offset < ( ts::Packet::SIZE - wrote_size - 3)) { // XXX: いろいろ
+    h.adaptation_field_exist |= 2;
+  }
+
+  size = h.dump(buf + wrote_size, sizeof(buf) - wrote_size);
+  wrote_size += size;
+
+  size_t rest_size = ts::Packet::SIZE - wrote_size;
+  if(payload.size() - data_offset < rest_size) {
+    // adaptation_field
+    ts::AdaptationField af;
+    af.discontinuity_indicator = false;
+    af.random_access_indicator = true;
+    af.es_priority_indicator =  false;
+    af.pcr_flag = false;
+    af.opcr_flag = false;
+    af.splicing_point_flag = false;
+    af.transport_private_data_flag = false;
+    af.adaptation_field_extension_flag = false;
+    
+    af.adaptation_field_length = 1;
+    
+    if(payload.size() - data_offset < rest_size - 2) { // 2 = adaptation-fieldの最小サイズ
+      size_t delta = rest_size - (payload.size() - data_offset) - 2;
+      af.adaptation_field_length += delta;
+    }
+
+    size = af.dump(buf + wrote_size, sizeof(buf) - wrote_size);
+    wrote_size += size;
+    rest_size  -= size;
+  }
+  out.write(reinterpret_cast<const char*>(buf), wrote_size);
+  
+  // pes: data
+  out.write(payload.data()+data_offset, rest_size);
+  data_offset += rest_size;
+}
+
+
+// TODO: write_videoを共通化
+void write_audio(const flv::Tag& tag, const std::string& payload, std::ostream& out) {
+  size_t data_offset=0;
+  write_audio_first(tag, payload, out, data_offset);
+  for(; data_offset < payload.size(); ) {
+    write_audio_rest(tag, payload, out, data_offset);
+  }
+}
+
+
+bool to_storage_format_sps_pps(const h264::AVCDecoderConfigurationRecord& conf, std::string& buf) {
+  for(uint8_t i=0; i < conf.num_of_sequence_parameter_sets; i++) {
+    // start code prefix
+    buf += (char)0;
+    buf += (char)0;
+    buf += (char)1;
+
+    buf += conf.sequence_parameter_set_list[i];
+  }
+  for(uint8_t i=0; i < conf.num_of_picture_parameter_sets; i++) {
+    // start code prefix
+    buf += (char)0;
+    buf += (char)0;
+    buf += (char)1;
+
+    buf += conf.picture_parameter_set_list[i];
+  }
+  return true;
+}
+
 bool to_storage_format(const h264::AVCDecoderConfigurationRecord& conf,
                        const uint8_t* data, size_t data_size, std::string& buf) {
   aux::ByteStream avc_in(data, data_size);
@@ -335,7 +521,9 @@ bool to_storage_format(const h264::AVCDecoderConfigurationRecord& conf,
     // start code prefix
     buf += (char)0;
     buf += (char)0;
+    buf += (char)0;
     buf += (char)1;
+
     buf.append(reinterpret_cast<const char*>(sample.nal_unit), sample.nal_unit_length);
   }
 
@@ -424,8 +612,12 @@ int main(int argc, char** argv) {
       adts::Header adts = adts::Header::make_default(tag.audio.payload_size);
       char buf[7];
       adts.dump(buf, 7);
-      //std::cout.write(buf, 7);
-      //std::cout.write(reinterpret_cast<const char*>(tag.audio.payload), tag.audio.payload_size);
+      
+      std::string buf2;
+      // buf2.assign(buf, 7);
+      buf2.append(reinterpret_cast<const char*>(tag.audio.payload), tag.audio.payload_size);
+      
+      write_audio(tag, buf2, ts_out);
       break;
     }
       
@@ -444,6 +636,12 @@ int main(int argc, char** argv) {
           std::cerr << "parse AVCDecoderConfigurationRecord failed" << std::endl;
           return 1;
         }
+
+        // XXX: これがあるとうまくいかない => なくても問題ない？
+        // std::string buf;
+        // to_storage_format_sps_pps(conf, buf); 
+        // write_video(tag, buf, ts_out);
+        
         break;
       }
       case 1: {
